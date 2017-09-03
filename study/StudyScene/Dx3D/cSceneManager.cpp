2246b92f146fe2967533d706a5c70640948b107d
LeeDongYoun <dkdla12@naver.com>
2017-09-03 14:20:09 +0900

    씬매니저 로딩 쓰레드 함수채움

--- study/StudyScene/Dx3D/cSceneManager.cpp
+++ study/StudyScene/Dx3D/cSceneManager.cpp
@@ -1,6 +1,26 @@
 #include "stdafx.h"
 #include "cSceneManager.h"
+#include "cMainGame.h"
 
+DWORD CALLBACK loadingThread(LPVOID prc)
+{
+	//교체해야 할 setup 함수 실행
+	cSceneManager::_readyScene->Setup();
+
+	//현재씬을 교체될 씬으로!!
+	cSceneManager::_currentScene = cSceneManager::_readyScene;
+
+	//해제...
+	cSceneManager::_loadingScene->~cMainGame();
+	cSceneManager::_loadingScene = NULL;
+	cSceneManager::_readyScene = NULL;
+
+	return NULL;
+}
+
+cMainGame* cSceneManager::_currentScene = NULL;
+cMainGame* cSceneManager::_loadingScene = NULL;
+cMainGame* cSceneManager::_readyScene = NULL;
 
 cSceneManager::cSceneManager()
 {
@@ -10,3 +30,31 @@ cSceneManager::cSceneManager()
 cSceneManager::~cSceneManager()
 {
 }
+
+HRESULT cSceneManager::Setup(void)
+{
+	return E_NOTIMPL;
+}
+
+void cSceneManager::release(void)
+{
+}
+
+void cSceneManager::update(void)
+{
+}
+
+void cSceneManager::render(void)
+{
+}
+
+cMainGame * cSceneManager::addScene(string sceneName, cMainGame * scene)
+{
+	return nullptr;
+}
+
+HRESULT cSceneManager::changeScene(string sceneName)
+{
+	return E_NOTIMPL;
+}
+
