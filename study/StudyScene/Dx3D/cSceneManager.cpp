6aecb7ed41179eff85ac2ee242840f823350b10a
LeeDongYoun <dkdla12@naver.com>
2017-09-03 14:21:55 +0900

    씬매니저 릴리즈 함수 채움

--- study/StudyScene/Dx3D/cSceneManager.cpp
+++ study/StudyScene/Dx3D/cSceneManager.cpp
@@ -42,6 +42,26 @@ HRESULT cSceneManager::Setup(void)
 
 void cSceneManager::release(void)
 {
+	//맵을 모두 돌면서 삭제..
+	mapSceneIter iter = _mSceneList.begin();
+
+	for (; iter != _mSceneList.end();)
+	{
+		//지울 수 있으면 반복자 증가 x
+		if (iter->second != NULL)
+		{
+			if (_currentScene == iter->second) iter->second->~cMainGame();
+			SAFE_DELETE(iter->second);
+			iter = _mSceneList.erase(iter);
+		}
+		else //반복자 증가!
+		{
+			++iter;
+		}
+	}
+
+	_currentScene = NULL;
+	_mSceneList.clear();
 }
 
 void cSceneManager::update(void)
