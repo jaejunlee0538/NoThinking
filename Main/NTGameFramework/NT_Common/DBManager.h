7e7c23fa2580f58406197c200c3955e31ff651fe
LeeDongYoun <dkdla12@naver.com>
2017-09-04 01:34:38 +0900

    헤더 std안붙어서 컴파일에러나는거 잡음

--- Main/NTGameFramework/NT_Common/DBManager.h
+++ Main/NTGameFramework/NT_Common/DBManager.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <NT_Common\Singleton.h>
+#include <vector>
+#include <string>
 
 namespace qwer {
 
@@ -7,14 +9,15 @@ namespace qwer {
 	{
 	public:
 
-		void save(const char* szFolder,const char* saveFileName, vector<string> vStr);
-		vector<string> load(const char* szFolder, const char* loadFileName);
+		void save(const char* szFolder,const char* saveFileName, std::vector<std::string> vStr);
+		std::vector<std::string> load(const char* szFolder, const char* loadFileName);
 
-		char* vectorArrayCombine(vector<string> vArray);
-		vector<string> charArraySeparation(char charArray[]);
+		char* vectorArrayCombine(std::vector<std::string> vArray);
+		std::vector<std::string> charArraySeparation(char charArray[]);
 
 		DBManager();
 		~DBManager();
 	};
 }
+#define GET_DBMANAGER() qwer::DBManager::GetSingleton()
 
